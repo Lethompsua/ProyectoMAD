3ba3d4a5fdd78c719c075b92f38ebd5de51184ea
Daniel Zambrano <110945749+danielz0102@users.noreply.github.com>
2024-05-03 17:39:58 -0600

    Registrar usuario (#1)
    * Validaciones de: nombre, correo, género, pregunta y respuesta, contraseña y edad.
    
    ---------
    
    Co-authored-by: Daniel Zambrano Gonzalez <daniel.zambranogn@uanl.edu.mx>

--- ProyectoMAD/Form2.Designer.cs
+++ ProyectoMAD/Form2.Designer.cs
@@ -47,94 +47,95 @@
             this.cbPregunta = new System.Windows.Forms.ComboBox();
             this.label8 = new System.Windows.Forms.Label();
             this.txtRespuesta = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(386, 74);
+            this.label1.Location = new System.Drawing.Point(273, 88);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(208, 27);
+            this.label1.Size = new System.Drawing.Size(164, 21);
             this.label1.TabIndex = 0;
             this.label1.Text = "Nombre (completo):";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(479, 114);
+            this.label2.Location = new System.Drawing.Point(366, 128);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(83, 27);
+            this.label2.Size = new System.Drawing.Size(66, 21);
             this.label2.TabIndex = 1;
             this.label2.Text = "Correo:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(444, 154);
+            this.label3.Location = new System.Drawing.Point(331, 168);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(124, 27);
+            this.label3.Size = new System.Drawing.Size(101, 21);
             this.label3.TabIndex = 2;
             this.label3.Text = "Contraseña:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(365, 194);
+            this.label4.Location = new System.Drawing.Point(252, 208);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(223, 27);
+            this.label4.Size = new System.Drawing.Size(180, 21);
             this.label4.TabIndex = 3;
             this.label4.Text = "Confirmar contraseña:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(393, 237);
+            this.label5.Location = new System.Drawing.Point(280, 251);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(187, 27);
+            this.label5.Size = new System.Drawing.Size(152, 21);
             this.label5.TabIndex = 4;
             this.label5.Text = "Fecha Nacimiento:";
             // 
             // txtNomCom
             // 
-            this.txtNomCom.Location = new System.Drawing.Point(568, 71);
+            this.txtNomCom.Location = new System.Drawing.Point(455, 85);
             this.txtNomCom.Name = "txtNomCom";
-            this.txtNomCom.Size = new System.Drawing.Size(274, 34);
+            this.txtNomCom.Size = new System.Drawing.Size(300, 29);
             this.txtNomCom.TabIndex = 5;
             // 
             // txtCorreo
             // 
-            this.txtCorreo.Location = new System.Drawing.Point(568, 111);
+            this.txtCorreo.Location = new System.Drawing.Point(455, 125);
             this.txtCorreo.Name = "txtCorreo";
-            this.txtCorreo.Size = new System.Drawing.Size(274, 34);
+            this.txtCorreo.Size = new System.Drawing.Size(300, 29);
             this.txtCorreo.TabIndex = 6;
             // 
             // txtConfContrasenaña
             // 
-            this.txtConfContrasenaña.Location = new System.Drawing.Point(568, 191);
+            this.txtConfContrasenaña.Location = new System.Drawing.Point(455, 205);
             this.txtConfContrasenaña.Name = "txtConfContrasenaña";
-            this.txtConfContrasenaña.Size = new System.Drawing.Size(274, 34);
+            this.txtConfContrasenaña.Size = new System.Drawing.Size(300, 29);
             this.txtConfContrasenaña.TabIndex = 8;
             // 
             // txtContrasena
             // 
-            this.txtContrasena.Location = new System.Drawing.Point(568, 151);
+            this.txtContrasena.Location = new System.Drawing.Point(455, 165);
             this.txtContrasena.Name = "txtContrasena";
-            this.txtContrasena.Size = new System.Drawing.Size(274, 34);
+            this.txtContrasena.Size = new System.Drawing.Size(300, 29);
             this.txtContrasena.TabIndex = 9;
             // 
             // DTPFechaNac
             // 
-            this.DTPFechaNac.Location = new System.Drawing.Point(568, 231);
+            this.DTPFechaNac.Location = new System.Drawing.Point(455, 245);
             this.DTPFechaNac.Name = "DTPFechaNac";
-            this.DTPFechaNac.Size = new System.Drawing.Size(274, 34);
+            this.DTPFechaNac.Size = new System.Drawing.Size(300, 29);
             this.DTPFechaNac.TabIndex = 10;
             // 
             // rbFem
             // 
             this.rbFem.AutoSize = true;
-            this.rbFem.Location = new System.Drawing.Point(571, 278);
+            this.rbFem.Location = new System.Drawing.Point(458, 292);
             this.rbFem.Name = "rbFem";
-            this.rbFem.Size = new System.Drawing.Size(127, 31);
+            this.rbFem.Size = new System.Drawing.Size(104, 25);
             this.rbFem.TabIndex = 11;
             this.rbFem.TabStop = true;
             this.rbFem.Text = "Femenino";
@@ -143,9 +144,9 @@
             // rbMas
             // 
             this.rbMas.AutoSize = true;
-            this.rbMas.Location = new System.Drawing.Point(571, 309);
+            this.rbMas.Location = new System.Drawing.Point(458, 323);
             this.rbMas.Name = "rbMas";
-            this.rbMas.Size = new System.Drawing.Size(130, 31);
+            this.rbMas.Size = new System.Drawing.Size(106, 25);
             this.rbMas.TabIndex = 12;
             this.rbMas.TabStop = true;
             this.rbMas.Text = "Masculino";
@@ -154,71 +155,85 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(475, 280);
+            this.label6.Location = new System.Drawing.Point(362, 294);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(87, 27);
+            this.label6.Size = new System.Drawing.Size(70, 21);
             this.label6.TabIndex = 13;
             this.label6.Text = "Genero:";
             // 
             // btnRegUsu
             // 
-            this.btnRegUsu.Location = new System.Drawing.Point(561, 505);
+            this.btnRegUsu.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnRegUsu.Location = new System.Drawing.Point(458, 499);
             this.btnRegUsu.Name = "btnRegUsu";
             this.btnRegUsu.Size = new System.Drawing.Size(113, 63);
             this.btnRegUsu.TabIndex = 14;
             this.btnRegUsu.Text = "Registrar Usuario";
-            this.btnRegUsu.UseVisualStyleBackColor = true;
+            this.btnRegUsu.UseVisualStyleBackColor = false;
             this.btnRegUsu.Click += new System.EventHandler(this.btnRegUsu_Click);
             // 
             // btnCancelar
             // 
-            this.btnCancelar.Location = new System.Drawing.Point(680, 505);
+            this.btnCancelar.BackColor = System.Drawing.Color.LightCoral;
+            this.btnCancelar.Location = new System.Drawing.Point(577, 499);
             this.btnCancelar.Name = "btnCancelar";
             this.btnCancelar.Size = new System.Drawing.Size(107, 63);
             this.btnCancelar.TabIndex = 15;
             this.btnCancelar.Text = "Cancelar";
-            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.UseVisualStyleBackColor = false;
             this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(365, 367);
+            this.label7.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(280, 375);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(678, 27);
+            this.label7.Size = new System.Drawing.Size(547, 21);
             this.label7.TabIndex = 16;
             this.label7.Text = "Por si su cuenta queda inhabilitada, escoja una pregunta de seguridad";
             // 
             // cbPregunta
             // 
             this.cbPregunta.FormattingEnabled = true;
-            this.cbPregunta.Location = new System.Drawing.Point(523, 407);
+            this.cbPregunta.Location = new System.Drawing.Point(343, 408);
             this.cbPregunta.Name = "cbPregunta";
-            this.cbPregunta.Size = new System.Drawing.Size(303, 35);
+            this.cbPregunta.Size = new System.Drawing.Size(454, 29);
             this.cbPregunta.TabIndex = 17;
-            this.cbPregunta.Text = "Pregunta";
+            this.cbPregunta.Text = "Selecciona una pregunta";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(430, 447);
+            this.label8.Location = new System.Drawing.Point(250, 448);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(108, 27);
+            this.label8.Size = new System.Drawing.Size(87, 21);
             this.label8.TabIndex = 18;
             this.label8.Text = "Respuesta";
             // 
             // txtRespuesta
             // 
-            this.txtRespuesta.Location = new System.Drawing.Point(523, 442);
+            this.txtRespuesta.Location = new System.Drawing.Point(343, 443);
             this.txtRespuesta.Name = "txtRespuesta";
-            this.txtRespuesta.Size = new System.Drawing.Size(303, 34);
+            this.txtRespuesta.Size = new System.Drawing.Size(454, 29);
             this.txtRespuesta.TabIndex = 19;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(306, 25);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(423, 31);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Ingrese sus datos para registrarse";
             // 
             // Form2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 27F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1200, 759);
+            this.ClientSize = new System.Drawing.Size(968, 597);
+            this.Controls.Add(this.label9);
             this.Controls.Add(this.txtRespuesta);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.cbPregunta);
@@ -241,7 +256,8 @@
             this.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form2";
-            this.Text = "Ingresa un usuario:";
+            this.Text = "Registrar Usuario";
+            this.Load += new System.EventHandler(this.Form2_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -268,5 +284,6 @@
         private System.Windows.Forms.ComboBox cbPregunta;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.TextBox txtRespuesta;
+        private System.Windows.Forms.Label label9;
     }
 }
